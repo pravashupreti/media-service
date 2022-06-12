018129a9006b9c0b3a94af4038306b9a6c9cdd79
pravash <pravash@pravash-mac.local>
2022-06-12 09:31:53 -0500

    Add repository

---
+++ src/main/java/com/ea/mediaservice/repository/AlbumRepository.java
@@ -0,0 +1,12 @@
+package com.ea.mediaservice.repository;
+
+import com.ea.mediaservice.model.Album;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AlbumRepository extends JpaRepository<Album, Long> {
+	Page<Album> findByUserId(Long userId, Pageable pageable);
+}
